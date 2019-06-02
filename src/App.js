aa9a6e797d3cea6d4d8b1936d08ed83b4230278b
Sumit <sumit2911997@gmail.com>
2019-06-03 00:55:42 +0530

    First Build

--- src/App.js
+++ src/App.js
@@ -1,26 +1,117 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Markdown from 'markdown-to-jsx';
 
-function App() {
+function Editor(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <textarea 
+      id='textarea'
+      className='text'
+      type='text'
+      onChange={props.handleChange}
+      value={placeholder}
+    />
   );
 }
+
+class Preview extends React.Component{
+  render(){
+    return(
+      <Markdown
+      className={this.props.className}
+      >
+        {this.props.markdown}
+      </Markdown>
+    );
+  }
+}
+
+class ToolBar extends React.Component {
+  render(){
+    return(
+      <div
+      className='toolbar-div'
+      id={this.props.name}
+      >
+        {this.props.name}
+      </div>
+    );
+  }
+}
+
+class NavigationBar extends React.Component{
+  render(){
+    return(
+      <div 
+      className={this.props.className}>
+        <h1>Markdown Previewer</h1>
+      </div>
+    );
+  }
+}
+
+class Content extends React.Component{
+  render(){
+    return(
+      <div
+      className={this.props.className}>
+        <ToolBar
+        name='Editor'
+        />
+        <Editor
+        className='editor'
+        handleChange={this.props.handleChange}
+        />
+        <ToolBar
+        name='Preview'
+        />
+        <Preview
+        className='preview'
+        markdown={this.props.markdown}
+        />
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      markdown:placeholder,
+    }
+  }
+
+  handleChange(){
+    this.setState({
+      markdown:document.getElementById('textarea').value,
+    });
+  }
+
+  render (){
+    return (
+      <div
+      className='App'>
+        <NavigationBar
+        className='nav-bar'
+        />
+        <Content
+        className='content-div'
+        markdown={this.state.markdown}
+        handleChange={this.handleChange.bind(this)}
+        />
+      </div>
+    );
+  }
+}
+
+const placeholder = 
+`# Welcome to my React Markdown Previewer!
+
+## Live editing will be provided very soon
+### Until then paste your markdown text below:
+`
 
 export default App;

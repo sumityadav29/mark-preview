0d11c8424fb5f0903b365d08e584db9bb7c6d0c1
Sumit <sumit2911997@gmail.com>
2019-06-03 00:56:52 +0530

    Updates

---
+++ precache-manifest.1f514003549bfac7fa3499350312486a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ad820de22e2f78d792b7666013af59",
+    "url": "/mark-preview/index.html"
+  },
+  {
+    "revision": "68b808ad8c1ff0aaa20b",
+    "url": "/mark-preview/static/css/main.5cd4b16c.chunk.css"
+  },
+  {
+    "revision": "580c00f8e4b895d2d1e9",
+    "url": "/mark-preview/static/js/2.fe39fb3e.chunk.js"
+  },
+  {
+    "revision": "68b808ad8c1ff0aaa20b",
+    "url": "/mark-preview/static/js/main.1d8c0950.chunk.js"
+  },
+  {
+    "revision": "9fca1d896a49653d2d77",
+    "url": "/mark-preview/static/js/runtime~main.c9e11cbe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mark-preview/static/media/logo.5d5d9eef.svg"
+  }
+]);
